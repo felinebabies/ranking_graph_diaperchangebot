3fd19defdb5075b0b9ff6ee84974ef51172aae94
Syuji <commentonly.is@gmail.com>
2016-02-14 23:56:18 +0900

    グラフ背景画像を引数で指定する機能を実装した。

--- pete_ranking_graph.rb
+++ pete_ranking_graph.rb
@@ -2,11 +2,27 @@
 require 'bundler'
 Bundler.require
 require 'yaml'
+require 'optparse'
 
 require_relative 'lib/bottwitterclient'
 
 currentdir = File.dirname(__FILE__)
 Dotenv.load
+
+# コマンドラインオプション解析
+def cmdline
+  args = {}
+
+  OptionParser.new do |parser|
+    parser.on('-g', '--graphonly', 'グラフ生成のみ') {|v| args[:graphonly] = v}
+    parser.on('-b PATH', '--bgimage', '背景画像を指定') {|v| args[:bgimage] = v}
+    parser.parse!(ARGV)
+  end
+
+  return args
+end
+
+args = cmdline
 
 #対象ファイル指定
 if(ARGV[0]) then
@@ -44,13 +60,31 @@ end
 g.data 'おむつ交換数', changeCount
 g.labels = labels
 
+# グラフのテーマ指定
+if args[:bgimage] && File.exists?(args[:bgimage]) then
+  bgImage = args[:bgimage]
+
+  defaultsTheme = {
+    :colors => ['orange', 'white'],
+    :additional_line_colors => [],
+    :marker_color => 'blue',
+    :font_color => 'blue',
+    :background_colors => nil,
+    :background_image => bgImage
+  }
+
+  g.theme = defaultsTheme
+end
+
 graphimgpath = File.join(currentdir,'ranking_graph.png')
 g.write(graphimgpath)
 
 sleep(1)
 
-#グラフを投稿する
-tweetText = ENV["TWEET_TEXT"] || "みんながぼくのおむつを交換してくれた回数を、パパがグラフにしてくれたよ。"
-File.open(graphimgpath, 'rb') do |f|
-  createclient.update_with_media(tweetText, f)
+unless args[:graphonly] then
+  #グラフを投稿する
+  tweetText = ENV["TWEET_TEXT"] || "みんながぼくのおむつを交換してくれた回数を、パパがグラフにしてくれたよ。"
+  File.open(graphimgpath, 'rb') do |f|
+    createclient.update_with_media(tweetText, f)
+  end
 end
